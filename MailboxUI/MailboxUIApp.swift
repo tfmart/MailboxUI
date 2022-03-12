ca341fb1e576a0f44a4cb7916e9fe93a3946f8d6
Tomás Martins <tomas.marti@me.com>
2022-03-11 23:29:11 -0300

    Initial Commit

---
+++ MailboxUI/MailboxUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailboxUIApp.swift
+//  MailboxUI
+//
+//  Created by Tomas Martins on 11/03/22.
+//
+
+import SwiftUI
+
+@main
+struct MailboxUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
