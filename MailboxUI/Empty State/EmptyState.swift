bc5f2516ce6796f1900514fabc9647cb21220cf6
Tomás Martins <tomas.marti@me.com>
2022-03-12 10:28:28 -0300

    Masked inbox icon

--- MailboxUI/Empty State/EmptyState.swift
+++ MailboxUI/Empty State/EmptyState.swift
@@ -28,19 +28,28 @@ struct EmptyState: View {
                 x: 0, y: 1)
     }
     
+    var inboxIcon: some View {
+        Image("mask")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(height: 172)
+            .mask({
+                Text("\(Image(systemName: "tray.fill"))")
+                    .font(.system(size: 144,
+                                  weight: .bold,
+                                  design: .default))
+            })
+            .shadow(color: .white.opacity(0.32),
+                    radius: 0,
+                    x: 0, y: 1)
+    }
+    
     var body: some View {
         ZStack {
             Color(red: 0.89, green: 0.89, blue: 0.89)
                 .edgesIgnoringSafeArea(.bottom)
             VStack {
-                Text("\(Image(systemName: "tray.fill"))")
-                    .font(.system(size: 144,
-                                  weight: .bold,
-                                  design: .default))
-                    .foregroundColor(.black.opacity(0.12))
-                    .shadow(color: .white.opacity(0.32),
-                            radius: 0,
-                            x: 0, y: 1)
+                inboxIcon
                 Text("You're all done.")
                     .foregroundColor(textColor)
                     .shadow(color: .white.opacity(0.48),
